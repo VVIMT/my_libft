e453b0b1947ad62efd37b040f2a576c12d86d08b
vinvimo <vinvimo@student.42.fr>
2017-05-19 23:26:34 +0200

    libft

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: vinvimo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 19:21:56 by vinvimo           #+#    #+#             */
-/*   Updated: 2017/05/19 22:52:53 by vinvimo          ###   ########.fr       */
+/*   Updated: 2017/05/19 23:23:07 by vinvimo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strnew(size_t size)
 	char	*x;
 	size_t	i;
 
-	if (!(x = (char *)malloc(sizeof(*x) * (size))))
+	if (!(x = (char *)malloc(sizeof(*x) * (size + 1))))
 		return (NULL);
 	i = 0;
 	while (i <= size)
